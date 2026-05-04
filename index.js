abd6d25b3938f74d016faf4bb00c59f6fd9d2e03
+ <+>
2026-05-04 19:35:39 -0400

    completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let counter = 0;
+
+let maxForRandom = 10;
+let random = Math.floor(Math.random() * maxForRandom) + 1;
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+function checkRemainder() {
+  if (mod === 4) {
+    console.log("Yes, the remainder is 4");
+  } else {
+    console.log("No, the remainder is not 4");
+  }
+}
+
+checkRemainder();
+
+let max = 20;
+console.log(Math.max(5,10,15,20));
